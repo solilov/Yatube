011e4c47d965fe4a113752d136af1e12d1c6672d
solilov <alexandr.solilov@yandex.ru>
2022-01-20 15:14:21 +0300

    test

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's@o&pt1ts4t@2#ktj@0ej+22^n664^4j1-b%*95*urql@qie!s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
